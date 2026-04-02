24d3283c14b428368809b7e0edbc2d98bf8579b4
First Commit, first thress questions <7631213+ss-kant@users.noreply.github.com>
2026-04-03 00:26:00 +0100

    initial docusaurus setup

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Home() {
+  return (
+    <Layout title="Hello" description="108-21 Blog">
+      <main style={{padding: '2rem', textAlign: 'center'}}>
+        <h1>Welcome to 108-21</h1>
+        <p>The blog is under construction.</p>
+      </main>
+    </Layout>
+  );
+}
