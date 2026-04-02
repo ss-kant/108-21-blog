91f935bec929b2909a6148eb841200ab6d9fecd3
First Commit, first thress questions <7631213+ss-kant@users.noreply.github.com>
2026-04-03 00:36:49 +0100

    inner folder deleted

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://www.108-21.com',
+  url: 'https://108-21-blog.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
